04ac55895149482962007aeaca5b34d6c31dc510
Vitus Schuhwerk <vitus.schuhwerk@gmail.com>
2025-08-28 13:26:45 +0200

    feat: update packages & gulpfile (closer to upsteam)

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,56 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync').create();
-var $            = require('gulp-load-plugins')();
-var autoprefixer = require('autoprefixer');
-var gsass        = require('gulp-sass')(require('node-sass'));
-// source maps
-var sourcemaps   = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass-embedded'));
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
 
-var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
+const includePaths = [
+	'node_modules/foundation-sites/scss',
+	'node_modules/motion-ui/src'
 ];
 
-function sass( path ) {
-  return gulp.src( path )
-    .pipe(sourcemaps.init()) // Initialize sourcemaps
-    .pipe(gsass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', gsass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemaps.write('.')) // Write sourcemaps
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
+
+function sassBuild(path) {
+	return gulp.src(path)
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			includePaths: includePaths,
+			outputStyle: 'compressed',
+			// Filter out specific deprecation warnings from Dart Sass
+			logger: {
+				warn: (warning) => {
+					const text = warning && warning.message ? warning.message : String(warning);
+					// Ignore the deprecated @import rule warning
+					if (text.includes('Sass @import rules are deprecated')) {
+						return;
+					}
+					if (text.includes('Global built-in functions are deprecated')) {
+						return;
+					}
+					if (text.includes("Sass's behavior for declarations that appear after nested")) {
+						return;
+					}
+					console.warn(text);
+				},
+				debug: (message) => {
+					const text = message && message.message ? message.message : String(message);
+					console.debug(text);
+				}
+			}
+		}).on('error', sass.logError))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.stream());
 };
 
-function sassFrontend(){
-  return sass('scss/app.scss');
-}
-function sassBackend(){
-  return sass('scss/app-backend.scss');
-}
-
-
 function serve() {
-  browserSync.init({
-    server: "./"
-  });
-
-  gulp.watch("scss/*.scss", sassFrontend);
-  gulp.watch("scss/*.scss", sassBackend);
-  gulp.watch("*.html").on('change', browserSync.reload);
+	browserSync.init({ server: "./" });
+	gulp.watch("scss/*.scss", gulp.series('sassFrontend', 'sassBackend'));
+	gulp.watch("*.html").on('change', browserSync.reload);
 }
 
-gulp.task('sassFrontend', sassFrontend);
-gulp.task('sassBackend', sassBackend);
+gulp.task('sassFrontend', () => sassBuild(['scss/app.scss']));
+gulp.task('sassBackend', () => sassBuild(['scss/app-backend.scss']));
 
+gulp.task('sass', gulp.series('sassFrontend', 'sassBackend', serve));
 gulp.task('serve', gulp.series('sassFrontend', 'sassBackend', serve));
 gulp.task('default', gulp.series('sassFrontend', 'sassBackend', serve));
